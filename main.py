8414460782cbf96b983505f9fdedb977d6ab451b
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-08 15:50:29 +0300

    Output as json

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 from sber_scraper import SberScraper
 from selenium_driver import SeleniumDriver
+import json
 
 
 def main(urls: list):
     driver = SeleniumDriver()
     scraper = SberScraper(driver.driver)
+    result = '['
     for url in urls:
         data = scraper.get_product(url.strip())
-        print(data)
+        result += json.dumps(str(data)) + ','
+    result = result[:-1] + ']'
+    print(result)
 
 if __name__ == '__main__':
     if sys.argv[1] == '-f' and sys.argv[2]:
