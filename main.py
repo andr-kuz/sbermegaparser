fed3ceca028dfea8369d7206a59a96b5bd64f127
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-11 10:28:42 +0300

    Crawling pause

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium_driver import SeleniumDriver
 import json
 
 
-def main(urls: list):
+def main(urls: list, pause: int | str = 0):
     driver = SeleniumDriver()
     scraper = SberScraper(driver.driver)
     result = '{'
@@ -12,13 +12,17 @@ def main(urls: list):
         url = url.strip()
         if data := scraper.get_product(url):
            result += '"' + url + '":' + json.dumps(str(data)) + ','
+           driver.driver.implicitly_wait(pause)
     result = result[:-1] + '}'
     print(result)
 
 if __name__ == '__main__':
+    pause = 0
+    if sys.argv[3] == '-p' and sys.argv[4]:
+        pause = sys.argv[4]
     if sys.argv[1] == '-f' and sys.argv[2]:
         with open(sys.argv[2]) as file:
             urls = list(file)
-            main(urls)
+            main(urls, pause)
     else:
         main([sys.argv[1]])
