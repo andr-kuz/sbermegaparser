53f7e1804b2c88de620466b9e970b1bc282dd59a
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-17 18:58:45 +0300

    added get_shop_name()

--- sber_product.py
+++ sber_product.py
@@ -11,6 +11,7 @@ class SberProduct:
             'url': self.get_url(),
             'price': self.get_price(),
             'cashback_percent': self.get_cashback_percent(),
+            'shop_name': self.get_shop_name(),
         })
 
     def get_price(self, raw = False) -> int | str | None:
@@ -31,4 +32,11 @@ class SberProduct:
             result = element.get_text().strip()
             if not raw and result:
                 result = int(result.split('%')[0])
+        return result
+
+    def get_shop_name(self, raw = False) -> int | str | None:
+        result = None
+        if element := self.soup.select_one('.pdp-offer-block__merchant-link'):
+            if result := element.get_text().strip():
+                return result
         return result
