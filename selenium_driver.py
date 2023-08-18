3b47eb5d40623be2c4e7a12e22c624071e445f91
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-18 14:42:07 +0300

    Changed args

--- selenium_driver.py
+++ selenium_driver.py
@@ -6,7 +6,7 @@ options.add_argument('--disable-gpu')
 
 
 class SeleniumDriver:
-    def __init__(self, proxy = ''):
+    def __init__(self, proxy_address = ''):
         sw_options = {
             'auto_config': False,
             'addr': 'sberparser',
@@ -16,12 +16,16 @@ class SeleniumDriver:
         proxy.http_proxy = 'sberparser:8087'
         proxy.ssl_proxy = 'sberparser:8087'
         options.proxy = proxy
-        if proxy:
+        if proxy_address:
             sw_options['proxy'] = {
-                'http': proxy,
-                'https': proxy,
+                'http': proxy_address,
+                'https': proxy_address,
             }
         self.driver = webdriver.Remote("http://firefox:4444/wd/hub", options=options, seleniumwire_options=sw_options)
+        # debug ip
+        # from selenium.webdriver.common.by import By
+        # self.driver.get('https://2ip.ru')
+        # print(self.driver.find_element(By.CSS_SELECTOR, '#d_clip_button').text)
 
     def __repr__(self):
         return self.driver
