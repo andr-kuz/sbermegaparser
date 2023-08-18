aefa8a4d3f8c5c9d38c9be59558a9ac6d2df064d
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-18 13:58:05 +0300

    Wait for logo to appear

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class Client:
     def __init__(self, driver: WebDriver):
@@ -6,4 +10,8 @@ class Client:
 
     def get(self, url: str):
         self.driver.get(url)
+        try:
+            WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR,'.header-logo'))).text
+        except TimeoutException:
+            pass
         return self.driver.page_source
