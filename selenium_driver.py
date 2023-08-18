b9c7ac673a291ab22cc40068632ba56bc9c7016c
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-18 14:01:27 +0300

    Selenium using proxy

--- selenium_driver.py
+++ selenium_driver.py
@@ -1,12 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from seleniumwire import webdriver
 
-options = Options()
+options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
 
 class SeleniumDriver:
-    def __init__(self):
-        self.driver = webdriver.Remote("http://firefox:4444/wd/hub", options=options)
+    def __init__(self, proxy = ''):
+        sw_options = {
+            'auto_config': False,
+            'addr': 'sberparser',
+            'port': 8087,
+        }
+        proxy = webdriver.Proxy()
+        proxy.http_proxy = 'sberparser:8087'
+        proxy.ssl_proxy = 'sberparser:8087'
+        options.proxy = proxy
+        if proxy:
+            sw_options['proxy'] = {
+                'http': proxy,
+                'https': proxy,
+            }
+        self.driver = webdriver.Remote("http://firefox:4444/wd/hub", options=options, seleniumwire_options=sw_options)
 
     def __repr__(self):
         return self.driver
