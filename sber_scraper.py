73320f373bd98d77f055f6bb852317144577b76d
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-24 18:33:31 +0300

    Renamed variable driver to client

--- sber_scraper.py
+++ sber_scraper.py
@@ -4,13 +4,13 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class SberScraper:
-    def __init__(self, driver: WebDriver):
-        self.driver = Client(driver)
+    def __init__(self, client: WebDriver):
+        self.client = Client(client)
         self.url_cache = {}
 
     def get_product(self, url) -> SberProduct:
         if not self.url_cache.get(url):
-            html = self.driver.get(url)
+            html = self.client.get(url)
             product = SberProduct(html)
             self.url_cache[url] = product
         return self.url_cache[url]
