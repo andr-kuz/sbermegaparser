be5630aa724d1c84c9baad9edd03aa8a385e7d65
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-08-24 11:41:55 +0300

    Fix for count_process

--- utils.py
+++ utils.py
@@ -1,17 +1,9 @@
 import psutil
-import os
 
 
 def count_process(path: str) -> int:
-    script_name = path.split(os.sep)[-1]
-    path_clear = path.split(script_name)[0][:-1]
     processes = []
     for process in psutil.process_iter():
-        try:
-            process_path = process.cwd()
-        except psutil.AccessDenied:
-            continue
-        if process_path == path_clear:
-            if script_name in process.cmdline():
-                processes.append(process)
+        if path in process.cmdline():
+            processes.append(process)
     return len(processes)
