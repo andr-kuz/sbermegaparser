37a116fc5be57a08a03a704f21bead51083157de
Andrey Kuznetsov laptop <awakum192@gmail.com>
2023-09-23 12:24:28 +0300

    Added cache

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ class ClientStalledException(Exception):
 class Client:
     def __init__(self, proxy_address: str):
         self.proxy = proxy_address
-        pass
+        self.cache = {}
 
     @abstractmethod
     def _get_data(self, url: str) -> str:
@@ -26,10 +26,12 @@ class Client:
         pass
 
     def get_product(self, url: str) -> Product:
-        html = self._get_data(url)
-        self._save_log(url, html)
-        self._is_client_stalled(html)
-        return Product(html)
+        if not url in self.cache:
+            html = self._get_data(url)
+            self._save_log(url, html)
+            self._is_client_stalled(html)
+            self.cache[url] = Product(html)
+        return self.cache[url]
 
     def _is_client_stalled(self, html: str):
         stalled_text = 'Запросы с вашего устройства похожи на автоматические'
