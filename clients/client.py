12787823a4185f6eee8eade2b4df577ff6408f16
andrey <awakum192@gmail.com>
2023-12-01 18:36:51 +0300

    Build: Client object name with proxy string

--- clients/client.py
+++ clients/client.py
@@ -4,6 +4,12 @@ class Client:
     def __init__(self, proxy_address: str):
         self.proxy = proxy_address
 
+    def __str__(self):
+        return f'<{self.__class__.__name__} {self.proxy}>'
+
+    def __repr__(self):
+        return self.__str__
+
     @abstractmethod
     def get(self, url: str, find_css_on_page: str | None = None) -> str:
         pass
