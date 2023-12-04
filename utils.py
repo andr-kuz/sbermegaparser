102a34a9f6310abb5d0b4cba232de5421ed31c0e
andrey <awakum192@gmail.com>
2023-12-05 01:02:54 +0300

    Fix

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import psutil
 def count_process(path: str) -> int:
     processes = []
     for process in psutil.process_iter():
-        print(process.cmdline())
         if path in process.cmdline():
             processes.append(process)
     return len(processes)
